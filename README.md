fa4d11066578a7eed3af684bbca47b519c19edde
Tamara Temple <tamouse@gmail.com>
2015-01-03 19:02:52 -0600

    add fontawesome and twitter_bootstrap_form_for gems

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 This template is how I like to have new rails applications set up.
 
+* *dump* spring
+* pry, with debugging and console support
+* rspec
+* factory girl
+* haml
+* twitter bootstrap, sass form
+* additional support for tbs horizontal form builders
+* startup page at root
+
 ## Usage:
 
     $ rails new --skip-bundle --skip-test-unit --skip-spring -m my_rails.rb
--- my_rails.rb
+++ my_rails.rb
@@ -2,9 +2,37 @@ gsub_file("app/views/layouts/application.html.erb", %r{, 'data-turbolinks-track'
 gsub_file("app/assets/javascripts/application.js", %r{^//= require turbolinks\n}, '')
 remove_file("app/assets/stylesheets/application.css")
 create_file("app/assets/stylesheets/application.css.scss") do
-  %q{// "bootstrap-sprockets" must be imported before "bootstrap" and "bootstrap/variables"
+  %q{
+// Set up template for sticky footer and fixed top navbar
+$footer_height: 60px;
+
+// "bootstrap-sprockets" must be imported before "bootstrap" and "bootstrap/variables"
 @import "bootstrap-sprockets";
-@import "bootstrap";}
+@import "bootstrap";
+
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    /* Set the fixed height of the footer here */
+    height: $footer_height;
+    background-color: #f5f5f5;
+
+    > .container {
+        padding-top: 20px;
+        padding-right: 15px;
+        padding-left:  15px;
+    }
+}
+
+body {
+    margin-bottom: $footer_height;
+
+    > .container {
+        padding: $navbar_height 15px;
+    }
+}
+}
 end
 append_to_file("app/assets/javascripts/application.js") do
   %q{//= require bootstrap-sprockets    
@@ -23,10 +51,12 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'unicorn'
 gem 'capistrano-rails', group: :development
 gem "bootstrap-sass"
+gem "fontawesome-rails"
+gem "twitter_bootstrap_form_for", github: "tamouse/twitter_bootstrap_form_for"
 
 gem_group :development, :test do
   gem 'pry-rails'
-  gem 'pry-rescue'
+  gem 'pry-byebug'
   gem 'awesome_print'
   gem 'rspec-rails'
   gem 'database_cleaner'
@@ -48,8 +78,8 @@ route("root 'static_pages#index'")
 remove_file("app/views/static_pages/index.html.haml")
 create_file("app/views/static_pages/index.html.haml") do
   %q{
-.container
-  .row
+.jumbotron
+  .container
     %h1 Welcome!
 
     %p Newly generated rails app with pry-rails, rspec, factory girl, haml, and twitter bootstrap already baked in!
@@ -59,8 +89,13 @@ end
 
 run("bundle exec html2haml app/views/layouts/application.html.erb app/views/layouts/application.html.haml")
 insert_into_file("app/views/layouts/application.html.haml",%q{
-    = render partial: "header"}, before: %r{\n.*= yield})
-append_to_file("app/views/layouts/application.html.haml", %q{    = render partial: "footer"
+    = render partial: "header"
+    = render partial: "flash_area"
+
+    .container
+  }, before: %r{\n.*= yield})
+append_to_file("app/views/layouts/application.html.haml", %q{
+    = render partial: "footer"
 })
 remove_file("app/views/layouts/application.html.erb")
 
@@ -73,7 +108,7 @@ create_file("app/views/application/_header.html.haml", %q{.navbar.navbar-default
         %span.icon-bar
         %span.icon-bar
         %span.icon-bar
-      %a.navbar-brand{href: "/"} New Rails App
+      %a.navbar-brand{href: root_path} New Rails App
 
     .collapse.navbar-collapse#navbar-collapse-1
       %ul.nav.navbar-nav
@@ -81,8 +116,36 @@ create_file("app/views/application/_header.html.haml", %q{.navbar.navbar-default
 
 })
 
-create_file("app/views/application/_footer.html.haml", %q{.well.text-center.text-muted Copyright &copy; Tamara Temple Web Development.
+create_file("app/views/application/_footer.html.haml", %Q{
+%footer.footer.bg-info
+  .container
+    .text-muted.text-center Copyright &copy; #{Time.now.year} Tamara Temple Web Development
 })
+
+create_file("app/views/application/_flash_area.html.haml", %Q{- if flash.any?
+  = flash_display
+})
+
+insert_into_file("app/helpers/application_helper.rb", %q{
+
+  def flash_display
+    content_tag(:div, class: "container") do
+      flash.each do |level, message|
+        concat(content_tag(:div, message, class: "alert alert-#{flash_level_to_bootstrap(level)}"))
+      end
+    end
+  end
+
+  def flash_level_to_bootstrap(level)
+    case level
+    when "alert"
+      "danger"
+    else
+      "info"
+    end
+  end
+
+}, before: "end")
 
 rake("db:create")
 rake("db:migrate")
@@ -99,10 +162,11 @@ A newly generated Rails application with the following already baked in:
 * factory girl
 * haml
 * twitter bootstrap
+* additional support for twitter bootstrap form building
 
 The database you've chosen has already been created.
 
-There is a static startup page at `/`.
+There is a static startup page at root.
 
 Tests are run with:
 
